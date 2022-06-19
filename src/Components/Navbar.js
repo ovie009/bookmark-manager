c8ae8fce5bac2bdd21efbdcda0ea99ef3e94c872
ovie009 <ovieiffieu2much@gmail.com>
2022-06-19 10:26:23 +0100

    navbar mobile styling completed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,13 +3,23 @@ import hamburger from '../images/icon-hamburger.svg'
 import close from '../images/icon-close.svg'
 import facebook from '../images/icon-facebook.svg';
 import twitter from '../images/icon-twitter.svg';
+import '../CSS/Navbar.css';
+import { useState } from 'react';
+
 
 const Navbar = () => {
+
+    const [sidebar, setSidebar] = useState(false);
+
+    const handleSidebar = () => {
+        setSidebar(!sidebar);
+    }
+
     return ( 
         <nav className="navbar">
             <img className="logo" src={logo} alt="Bookmark Icon" />
-            <button type='button'>
-                <img src={hamburger} alt="" />
+            <button type='button' className='open-sidenav' onClick={handleSidebar}>
+                <img src={hamburger} alt="hamburger icon" />
             </button>
             <ul className='nav-list'>
                 <li className="nav-list-items">
@@ -25,24 +35,42 @@ const Navbar = () => {
                     login
                 </li>
             </ul>
-            <div className="sidebar">
+            <div className="sidebar" style={{
+                left: `${sidebar ? '0px' : '100vw'}`
+            }}>
                 <div className="sidebar-main">
-                    <span>
-                        <img className="logo" src={logo} alt="Bookmark Icon" />
-                        <img src={close} alt="colse icon" />
-                    </span>
+                    <div className='sidebar-header'>
+                        <span className="sidebar-logo">
+                            <div className="sidebar-icon-wrapper">
+                                <ion-icon name="bookmark"></ion-icon>
+                            </div>
+                            BOOKMARK
+                        </span>
+                        {/* <img className="sidebar-logo" src={logo} alt="Bookmark Icon" /> */}
+                        <button type='button' className='close-sidenav' onClick={handleSidebar}>
+                            <img src={close} alt="close icon" />
+                        </button>
+                    </div>
                     <ul className="sidebar-list">
                         <li className="sidebar-list-items">
-                            FEATURES
+                            <button type="button">
+                                FEATURES
+                            </button>
                         </li>
                         <li className="sidebar-list-items">
-                            PRICING
+                            <button type="button">
+                               PRICING
+                            </button>
                         </li>
                         <li className="sidebar-list-items">
-                            CONTACT
+                            <button type="button">
+                                CONTACT
+                            </button>
                         </li>
                         <li className="sidebar-list-items">
-                            LOGIN
+                            <button type="button">
+                                LOGIN
+                            </button>
                         </li>
                     </ul>
                 </div>
